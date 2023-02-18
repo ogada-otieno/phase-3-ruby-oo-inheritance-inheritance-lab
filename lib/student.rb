65ee486f9909c1cd22e2e8e4819bf59645031598
Rodgers Ogada <ogadarodgers@gmail.com>
2023-02-18 11:42:56 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+require_relative './user/'
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(string)
+        knowledge << string
+    end
 end
